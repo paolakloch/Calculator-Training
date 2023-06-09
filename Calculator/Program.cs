3fc001372bb391286dadf62e9b2c4cb9bc5277e6
Paola K <kloch.157@gmail.com>
2023-06-09 09:24:26 -0300

    first commit

---
+++ Calculator/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Type a number");
+var firstNumber = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Type the operation");
+var operation = Console.ReadLine();
+
+Console.WriteLine("Type a number");
+var secondNumber = Convert.ToInt32(Console.ReadLine());
+
+if (operation == "+")
+    Console.WriteLine(firstNumber + secondNumber);
+
+if (operation == "-")
+   Console.WriteLine(firstNumber - secondNumber);
+
+if (operation == "x")
+    Console.WriteLine(firstNumber * secondNumber);
+
+if (operation == "/")
+    Console.WriteLine(firstNumber / secondNumber);
